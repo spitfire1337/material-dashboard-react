431937bd2c84cbc0524ef1354660b80c67a1b314
Mike <skatermike21988@yahoo.com>
2024-12-19 11:46:38 -0500

    sync

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   server = "http://localhost:3006";
   redirecturl = "http://localhost:3000";
 } else {
-  server = "https://api.pevconnection.com";
-  redirecturl = "https://api.pevconnection.com";
+  server = "https://crm.pevconnection.com";
+  redirecturl = "https://crm.pevconnection.com";
 }
 let vars = { serverUrl: server, redirect: redirecturl };
 export default vars;
